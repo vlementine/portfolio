7fe9ab8271140dbfd16290bb455a5ebdf7381597
Clementine Bertrand <bertrand_clementine@yahoo.fr>
2018-03-18 20:19:13 +0100

    Chat working !!!

--- script.js
+++ script.js
@@ -1,50 +1,63 @@
+//-------------------------------------
+//	VARIABLES
+//-------------------------------------
 var projectNumber = document.querySelectorAll('.project').length;
 document.querySelector('.number__total span:last-child').textContent = projectNumber;
 
-document.querySelector('body').onscroll = function(){
-  alert('PLOP');
-}
+//-------------------------------------
+//	GET ANSWER
+//-------------------------------------
+function getAnswer(name, groupBubble, nbBubble) {
+  var answer = document.getElementById(name + 'Answer').value;
+  displayBubble(groupBubble, nbBubble);
 
+  if (answer !== '' && answer !== ' ') {
+    document.getElementById(name + 'Answer').setAttribute('readonly', true);
 
-function ttdonnees(param1,param2,param3) {
-  var donnee = document.getElementById(param1+'Answer').value;
-
-  if(donnee !== "" && donnee !== " ") {
-      document.getElementById(param1+'Answer').setAttribute('readonly', true)
-      document.getElementById(param1).innerHTML = donnee ;
-      displayBubble(param2,param3);
-  } else {
-
-  }
-}
-
-function displayBubble(param,param2) {
-  document.querySelector('.contact__form form').scrollTo(0,document.body.scrollHeight);
-  document.querySelector('.bubble--group:nth-child('+ param +') .bubble').classList.add('enable');
-  document.querySelector('.bubble--group:nth-child('+ param +') .bubble--answer').classList.add('enable');
-  if(param2==3) {
-    document.querySelectorAll('.bubble--group:nth-child('+ param +') .bubble__choice .bubble').length;
-    for(var n =0; n<document.querySelectorAll('.bubble--group:nth-child('+ param +') .bubble__choice .bubble').length; n++) {
-      document.querySelectorAll('.bubble--group:nth-child('+ param +') .bubble__choice .bubble')[n].classList.add('enableBis')
+    if (name == 'name') {
+      document.getElementById(name + 'Question').innerHTML = answer;
     }
   }
 }
 
+//-------------------------------------
+//	DISPLAY BUBBLE
+//-------------------------------------
+function displayBubble(groupBubble, nbBubble) {
+  for (var n = 0; n < 2; n++) {
+    document.querySelectorAll('.bubble--group:nth-child(' + groupBubble + ') .bubble')[n].classList.add('enable');
+    document.querySelector('.contact__form form').scrollTo(0, document.body.scrollHeight);
+  }
+
+  if (nbBubble == 3) {
+    for (var n = 0; n < nbBubble; n++) {
+      document.querySelectorAll('.bubble--group:nth-child(' + groupBubble + ') .bubble--group__choice .bubble')[n].classList.add('enable__choice');
+      document.querySelector('.contact__form form').scrollTo(0, document.body.scrollHeight);
+    }
+  }
+}
+
+//-------------------------------------
+//	DISPLAY PROJECT
+//-------------------------------------
 function displayProject(project) {
-  var imgNumber = document.querySelectorAll(project +  ' .project__visuel img').length;
+  var imgNumber = document.querySelectorAll(project + ' .project__visuel img').length;
   //console.log(project + ' : ' + imgNumber);
-  for(var i = 0; i < imgNumber; i++){
-    document.querySelectorAll(project +  ' .project__visuel img')[i].classList.toggle('enable');
+  for (var i = 0; i < imgNumber; i++) {
+    document.querySelectorAll(project + ' .project__visuel img')[i].classList.toggle('enable');
   }
-  document.querySelector(project +  ' .project__visuel').classList.toggle('enable');
-  document.querySelector(project +  ' .description__number').classList.toggle('enable');
-  document.querySelector(project +  ' .description__title h2').classList.toggle('enable');
-  for(var j = 0; j < 3; j++){
-    document.querySelectorAll(project +  ' .info span')[j].classList.toggle('enable');
-    document.querySelectorAll(project +  ' .info p')[j].classList.toggle('enable');
+
+  document.querySelector(project + ' .project__visuel').classList.toggle('enable');
+  document.querySelector(project + ' .description__number').classList.toggle('enable');
+  document.querySelector(project + ' .description__title h2').classList.toggle('enable');
+
+  for (var j = 0; j < 3; j++) {
+    document.querySelectorAll(project + ' .info span')[j].classList.toggle('enable');
+    document.querySelectorAll(project + ' .info p')[j].classList.toggle('enable');
   }
-  document.querySelector(project +  ' .description p').classList.toggle('enable');
-  document.querySelector(project +  ' .description__wrapper .btn').classList.toggle('enable');
+
+  document.querySelector(project + ' .description p').classList.toggle('enable');
+  document.querySelector(project + ' .description__wrapper .btn').classList.toggle('enable');
 }
 
 function seeMyWork() {
@@ -59,17 +72,17 @@ function seeMyWork() {
 function navigationProjects(parameter) {
   var currentProjectNumber = parseInt(document.querySelector('.number__active span:last-child').innerHTML);
 
-  if ((currentProjectNumber+parameter) <= projectNumber && (currentProjectNumber+parameter) > 0) {
+  if ((currentProjectNumber + parameter) <= projectNumber && (currentProjectNumber + parameter) > 0) {
     //Dispay the current
     document.querySelector('.number__active span:last-child').textContent = currentProjectNumber + parameter;
 
-    var projectNext = '#project-0'+ (currentProjectNumber + parameter);
-    var projectPrev = '#project-0'+ currentProjectNumber;
+    var projectNext = '#project-0' + (currentProjectNumber + parameter);
+    var projectPrev = '#project-0' + currentProjectNumber;
 
     //Hide previous project
     displayProject(projectPrev);
 
-    setTimeout(function(){
+    setTimeout(function () {
       document.querySelector(projectPrev).classList.toggle('enable');
       document.querySelector(projectNext).classList.toggle('enable');
 
@@ -77,38 +90,88 @@ function navigationProjects(parameter) {
       displayProject(projectNext);
     }, 800);
   } else if ((currentProjectNumber) === 1) {
-      //Display Home
-      document.querySelector('.home').classList.toggle('disable');
+    //Display Home
+    document.querySelector('.home').classList.toggle('disable');
 
-      //Hide first project
-      displayProject('#project-01');
-      setTimeout(function(){
-        document.querySelector('#project-01').classList.toggle('enable');
-      }, 700);
+    //Hide first project
+    displayProject('#project-01');
+    setTimeout(function () {
+      document.querySelector('#project-01').classList.toggle('enable');
+    }, 700);
   } else if ((currentProjectNumber) === projectNumber) {
     //Display Contact
     document.querySelector('.contact__wrapper').classList.toggle('disable');
 
     //Hide last project
-    setTimeout(function(){
-      document.querySelector('#project-0'+ currentProjectNumber).classList.toggle('enable');document.querySelector('.bubble--group:nth-child(1) .bubble').classList.add('enable');
+    setTimeout(function () {
+      document.querySelector('#project-0' + currentProjectNumber).classList.toggle('enable');
+      document.querySelector('.bubble--group:nth-child(1) .bubble').classList.add('enable');
       document.querySelector('.bubble--group:nth-child(1) .bubble--answer').classList.add('enable');
 
     }, 700);
   }
 }
 
+//-------------------------------------
+//	VALIDATE EMAIL
+//-------------------------------------
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+function validate(groupBubble, nbBubble) {
+  var result = document.querySelector('#result');
+  var email = document.querySelector('#mailAnswer').value;
+
+  if (validateEmail(email)) {
+    document.querySelector('#mailAnswer').setAttribute('readonly', true);
+    result.style.display = 'none';
+    displayBubble(groupBubble, nbBubble);
+    sendEmail();
+  } else {
+    result.innerText = 'L\'adresse n\'est pas valide';
+    result.style.color = 'red';
+  }
+  return false;
+}
+
+//-------------------------------------
+//	MAIL SENDING
+//-------------------------------------
+function sendEmail() {
+  //Variables
+  var nameAnswer = document.getElementById('nameAnswer').value;
+  var mailAnswer = document.getElementById('mailAnswer').value;
+  if (document.getElementById('project-1').checked) {
+    var objectAnswer = "Une question ?";
+  } else if (document.getElementById('project-2').checked) {
+    var objectAnswer = "Un projet dans les cartons";
+  } else if (document.getElementById('project-3').checked) {
+    var objectAnswer = "Seulement envie de dire bonjour";
+  }
+  var messageAnswer = document.getElementById('messageAnswer').value;
+
+  console.log(nameAnswer + ' / ' + mailAnswer + ' / ' + objectAnswer + ' / ' + messageAnswer);
+
+  $.ajax({
+      url: 'sendEmail.php',
+      data: {
+        name: nameAnswer, 
+        mail: mailAnswer, 
+        object: objectAnswer, 
+        message: messageAnswer
+      }
+    }).done(function () {
+        console.log('Email sended');
+  });
+}
+
+//-------------------------------------
+//	MAIN FUNCTION
+//-------------------------------------
 function main() {
   document.querySelector('.btn__work').addEventListener('click', seeMyWork);
 }
 
 document.onload = main();
-
-function sendEmail() {
-  $.ajax({
-      url: "test.php",
-      data: {mail: "dsqfdqs@mail.fr", objet: "fdshifuhs fdhsufh usdgf udsgf usyd"}
-  }).done(function () {
-      console.log("HIUdfgds");
-  });
-}
---
+++ sendEmail.php
@@ -0,0 +1,89 @@
+<?php
+
+// --- Variables
+$name=$_GET["name"];
+$email=$_GET["mail"];
+$object=$_GET["object"];
+$message=$_GET["message"];
+
+// --- Recipient
+$mailRecipient = 'bertrand_clementine@yahoo.fr'; 
+
+// --- Message
+$message_html = '<html>
+<head>
+</head>
+
+<body>
+    <table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%">
+        <tr>
+            <td align="center" valign="top">
+                <table border="0" cellpadding="20" cellspacing="0" width="100%" style="font-family: Helvetica, Arial, sans-serif; color: #6A6C82; font-size: 14px; border: 1px solid #E6E6EA; border-radius: 2px; padding: 50px; margin: 30px auto; max-width: 600px;">
+                    <tr>
+                        <td align="center" style="padding: 0;">
+                            <h1 style="font-size: 48px; color: #000; margin: 0;">🍊</h1>
+                            <h2 style="font-size: 13px; font-weight: bold; color: #B4B5C0; text-transform: uppercase; margin: 5px 0 0 0;">What\'s New in your portfolio</h2>
+                            <h3 style="font-size: 22px; font-weight: bold; margin: 15px 0 40px 0; color: #383B58;">Nouvelle notification de ' . $name . '</h3>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="padding: 0;">
+                            <span style="font-weight: bold; color: #383B58;">Nom.</span>
+                            <p style="margin: 5px 0 20px 0">' . $name . '</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="padding: 0;">
+                            <span style="font-weight: bold; color: #383B58;">Objet.</span>
+                            <p style="margin: 5px 0 20px 0">' . $object . '</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="padding: 0;">
+                            <span style="font-weight: bold; color: #383B58;">Message.</span>
+                            <p style="margin: 5px 0 20px 0">' . $message . '</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="padding: 0;">
+                            <span style="font-weight: bold; color: #383B58;">Email.</span>
+                            <a href="mailto:' . $email . '" style="display: block; margin: 5px 0 0 0; color: #2286B6;">' . $email . '</a>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+</body>
+
+</html>';
+
+// --- Filter of bugs
+if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mailRecipient))
+{
+	$passage_ligne = "\r\n";
+}
+else
+{
+	$passage_ligne = "\n";
+}
+
+// --- Object
+$sujet = "Notification";
+
+// --- Header of email
+$header = "From: \"Clementine's portfolio\"<hello@clementinebertrand.fr>".$passage_ligne;
+//$header.= "Reply-to: \"Clem\" <plop@mail.fr>".$passage_ligne;
+$header.= "MIME-Version: 1.0".$passage_ligne;
+$header.= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
+
+// --- Message creation
+$message.= $passage_ligne."--".$boundary.$passage_ligne;
+$message.= "Content-Type: text/html; charset=\"UTF-8\"".$passage_ligne;
+$message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
+$message.= $message_html;
+
+// --- Send mail
+mail($mailRecipient,$sujet,$message,$header);
+
+?>
