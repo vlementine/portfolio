a6247efb7ff7f2de3e03b215a7b61825e9a40a24
Clementine Bertrand <bertrand_clementine@yahoo.fr>
2018-03-21 20:11:02 +0100

    Add Json

--- script.js
+++ script.js
@@ -1,21 +1,106 @@
 //-------------------------------------
+//	JSON
+//-------------------------------------
+const projects = [
+  {
+    number: "01",
+    title: "Back to your dream",
+    role: "Designer",
+    client: "IESA Multimédia",
+    year: "2016",
+    description:
+      "Option A, you kill me right here and now. Apparently I've made that very easy for you. You can kill me, no witnesses and then spend the next few weeks or months tracking down Jesse Pinkman and you kill him too. A pointless exercise it seems…",
+    image: "img/.png",
+    letter: "img/.png"
+  },
+  {
+    number: "02",
+    title: "Alcatraz, 1962",
+    role: "Designer",
+    client: "IESA Multimédia",
+    year: "2016",
+    description:
+      "Option A, you kill me right here and now. Apparently I've made that very easy for you. You can kill me, no witnesses and then spend the next few weeks or months tracking down Jesse Pinkman and you kill him too. A pointless exercise it seems…",
+    image: "img/.png",
+    letter: "img/.png"
+  },
+  {
+    number: "03",
+    title: "DISGQDOYUsqd, 1962",
+    role: "Designer",
+    client: "IESA Multimédia",
+    year: "2016",
+    description:
+      "Option A, you kill me right here and now. Apparently I've made that very easy for you. You can kill me, no witnesses and then spend the next few weeks or months tracking down Jesse Pinkman and you kill him too. A pointless exercise it seems…",
+    image: "img/.png",
+    letter: "img/.png"
+  }
+];
+
+//Add href, and alt for img
+
+let content = "";
+
+projects.forEach(project => {
+  content += `<div class="project" id="project-${project.number}">
+      <div class="project__visuel">
+        <div class="visuel__mockup">
+          <img class="mockup--desktop" src="img/btyd-visuel-home.png" alt="Visuel du Blog Zenchef" draggable="false">
+        </div>
+        <img src="img/btyd-letter.png" alt="B" draggable="false">
+      </div>
+
+      <div class="project__description">
+        <div class="description__wrapper">
+          <div class="description__title">
+            <h2> ${project.title} </h2>
+          </div>
+          <span class="description__number">${project.number}.</span>
+          <div class="description__info">
+            <div class="info">
+              <span>Rôle</span>
+              <p>${project.role}</p>
+            </div>
+            <div class="info">
+              <span>Client</span>
+              <p>${project.client}</p>
+            </div>
+            <div class="info">
+              <span>Année</span>
+              <p>${project.year}</p>
+            </div>
+          </div>
+          <div class="description">
+            <p>${project.description}</p>
+          </div>
+          <a class="btn" href="">Découvrir le projet</a>
+        </div>
+      </div>
+    </div>`;
+});
+
+console.log(content);
+
+document.getElementById("projects").innerHTML = content;
+
+//-------------------------------------
 //	VARIABLES
 //-------------------------------------
-var projectNumber = document.querySelectorAll('.project').length;
-document.querySelector('.number__total span:last-child').textContent = projectNumber;
+let projectNumber = document.querySelectorAll(".project").length;
+document.querySelector(".number__total span:last-child").textContent = projectNumber;
 
 //-------------------------------------
 //	GET ANSWER
 //-------------------------------------
 function getAnswer(name, groupBubble, nbBubble) {
-  var answer = document.getElementById(name + 'Answer').value;
+  let answer = document.getElementById(name + "Answer").value;
   displayBubble(groupBubble, nbBubble);
 
-  if (answer !== '' && answer !== ' ') {
-    document.getElementById(name + 'Answer').setAttribute('readonly', true);
+  if (answer !== "" && answer !== " ") {
+    document.getElementById(name + "Answer").setAttribute("readonly", true);
 
-    if (name == 'name') {
-      document.getElementById(name + 'Question').innerHTML = answer;
+    if (name == "name") {
+      document.getElementById(name + "Question").innerHTML = answer;
     }
   }
 }
@@ -24,15 +109,17 @@ function getAnswer(name, groupBubble, nbBubble) {
 //	DISPLAY BUBBLE
 //-------------------------------------
 function displayBubble(groupBubble, nbBubble) {
-  for (var n = 0; n < 2; n++) {
-    document.querySelectorAll('.bubble--group:nth-child(' + groupBubble + ') .bubble')[n].classList.add('enable');
-    document.querySelector('.contact__form form').scrollTo(0, document.body.scrollHeight);
+  for (let n = 0; n < 2; n++) {
+    document.querySelectorAll(".bubble--group:nth-child(" + groupBubble + ") .bubble")[n].classList.add("enable");
+    document.querySelector(".contact__form form").scrollTo(0, document.body.scrollHeight);
   }
 
   if (nbBubble == 3) {
-    for (var n = 0; n < nbBubble; n++) {
-      document.querySelectorAll('.bubble--group:nth-child(' + groupBubble + ') .bubble--group__choice .bubble')[n].classList.add('enable__choice');
-      document.querySelector('.contact__form form').scrollTo(0, document.body.scrollHeight);
+    for (let n = 0; n < nbBubble; n++) {
+      document
+        .querySelectorAll(".bubble--group:nth-child(" + groupBubble + ") .bubble--group__choice .bubble")
+        [n].classList.add("enable__choice");
+      document.querySelector(".contact__form form").scrollTo(0, document.body.scrollHeight);
     }
   }
 }
@@ -41,73 +128,71 @@ function displayBubble(groupBubble, nbBubble) {
 //	DISPLAY PROJECT
 //-------------------------------------
 function displayProject(project) {
-  var imgNumber = document.querySelectorAll(project + ' .project__visuel img').length;
-  //console.log(project + ' : ' + imgNumber);
-  for (var i = 0; i < imgNumber; i++) {
-    document.querySelectorAll(project + ' .project__visuel img')[i].classList.toggle('enable');
+  let imgNumber = document.querySelectorAll(project + " .project__visuel img").length;
+  for (let i = 0; i < imgNumber; i++) {
+    document.querySelectorAll(project + " .project__visuel img")[i].classList.toggle("enable");
   }
 
-  document.querySelector(project + ' .project__visuel').classList.toggle('enable');
-  document.querySelector(project + ' .description__number').classList.toggle('enable');
-  document.querySelector(project + ' .description__title h2').classList.toggle('enable');
+  document.querySelector(project + " .project__visuel").classList.toggle("enable");
+  document.querySelector(project + " .description__number").classList.toggle("enable");
+  document.querySelector(project + " .description__title h2").classList.toggle("enable");
 
-  for (var j = 0; j < 3; j++) {
-    document.querySelectorAll(project + ' .info span')[j].classList.toggle('enable');
-    document.querySelectorAll(project + ' .info p')[j].classList.toggle('enable');
+  for (let j = 0; j < 3; j++) {
+    document.querySelectorAll(project + " .info span")[j].classList.toggle("enable");
+    document.querySelectorAll(project + " .info p")[j].classList.toggle("enable");
   }
 
-  document.querySelector(project + ' .description p').classList.toggle('enable');
-  document.querySelector(project + ' .description__wrapper .btn').classList.toggle('enable');
+  document.querySelector(project + " .description p").classList.toggle("enable");
+  document.querySelector(project + " .description__wrapper .btn").classList.toggle("enable");
 }
 
 function seeMyWork() {
   //Hide Home
-  document.querySelector('.home').classList.add('disable');
+  document.querySelector(".home").classList.add("disable");
 
   //Display first project
-  document.querySelector('#project-01').classList.toggle('enable');
-  displayProject('#project-01');
+  document.querySelector("#project-01").classList.toggle("enable");
+  displayProject("#project-01");
 }
 
 function navigationProjects(parameter) {
-  var currentProjectNumber = parseInt(document.querySelector('.number__active span:last-child').innerHTML);
+  let currentProjectNumber = parseInt(document.querySelector(".number__active span:last-child").innerHTML);
 
-  if ((currentProjectNumber + parameter) <= projectNumber && (currentProjectNumber + parameter) > 0) {
+  if (currentProjectNumber + parameter <= projectNumber && currentProjectNumber + parameter > 0) {
     //Dispay the current
-    document.querySelector('.number__active span:last-child').textContent = currentProjectNumber + parameter;
+    document.querySelector(".number__active span:last-child").textContent = currentProjectNumber + parameter;
 
-    var projectNext = '#project-0' + (currentProjectNumber + parameter);
-    var projectPrev = '#project-0' + currentProjectNumber;
+    let projectNext = "#project-0" + (currentProjectNumber + parameter);
+    let projectPrev = "#project-0" + currentProjectNumber;
 
     //Hide previous project
     displayProject(projectPrev);
 
-    setTimeout(function () {
-      document.querySelector(projectPrev).classList.toggle('enable');
-      document.querySelector(projectNext).classList.toggle('enable');
+    setTimeout(function() {
+      document.querySelector(projectPrev).classList.toggle("enable");
+      document.querySelector(projectNext).classList.toggle("enable");
 
       //Display next project
       displayProject(projectNext);
     }, 800);
-  } else if ((currentProjectNumber) === 1) {
+  } else if (currentProjectNumber === 1) {
     //Display Home
-    document.querySelector('.home').classList.toggle('disable');
+    document.querySelector(".home").classList.toggle("disable");
 
     //Hide first project
-    displayProject('#project-01');
-    setTimeout(function () {
-      document.querySelector('#project-01').classList.toggle('enable');
+    displayProject("#project-01");
+    setTimeout(function() {
+      document.querySelector("#project-01").classList.toggle("enable");
     }, 700);
-  } else if ((currentProjectNumber) === projectNumber) {
+  } else if (currentProjectNumber === projectNumber) {
     //Display Contact
-    document.querySelector('.contact__wrapper').classList.toggle('disable');
+    document.querySelector(".contact__wrapper").classList.toggle("disable");
 
     //Hide last project
-    setTimeout(function () {
-      document.querySelector('#project-0' + currentProjectNumber).classList.toggle('enable');
-      document.querySelector('.bubble--group:nth-child(1) .bubble').classList.add('enable');
-      document.querySelector('.bubble--group:nth-child(1) .bubble--answer').classList.add('enable');
-
+    setTimeout(function() {
+      document.querySelector("#project-0" + currentProjectNumber).classList.toggle("enable");
+      document.querySelector(".bubble--group:nth-child(1) .bubble").classList.add("enable");
+      document.querySelector(".bubble--group:nth-child(1) .bubble--answer").classList.add("enable");
     }, 700);
   }
 }
@@ -116,22 +201,22 @@ function navigationProjects(parameter) {
 //	VALIDATE EMAIL
 //-------------------------------------
 function validateEmail(email) {
-  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
 }
 
 function validate(groupBubble, nbBubble) {
-  var result = document.querySelector('#result');
-  var email = document.querySelector('#mailAnswer').value;
+  let result = document.querySelector("#result");
+  let email = document.querySelector("#mailAnswer").value;
 
   if (validateEmail(email)) {
-    document.querySelector('#mailAnswer').setAttribute('readonly', true);
-    result.style.display = 'none';
+    document.querySelector("#mailAnswer").setAttribute("readonly", true);
+    result.style.display = "none";
     displayBubble(groupBubble, nbBubble);
     sendEmail();
   } else {
-    result.innerText = 'L\'adresse n\'est pas valide';
-    result.style.color = 'red';
+    result.innerText = "L'adresse n'est pas valide";
+    result.style.color = "red";
   }
   return false;
 }
@@ -141,29 +226,27 @@ function validate(groupBubble, nbBubble) {
 //-------------------------------------
 function sendEmail() {
   //Variables
-  var nameAnswer = document.getElementById('nameAnswer').value;
-  var mailAnswer = document.getElementById('mailAnswer').value;
-  if (document.getElementById('project-1').checked) {
-    var objectAnswer = "Une question ?";
-  } else if (document.getElementById('project-2').checked) {
-    var objectAnswer = "Un projet dans les cartons";
-  } else if (document.getElementById('project-3').checked) {
-    var objectAnswer = "Seulement envie de dire bonjour";
+  let nameAnswer = document.getElementById("nameAnswer").value;
+  let mailAnswer = document.getElementById("mailAnswer").value;
+  if (document.getElementById("project-1").checked) {
+    let objectAnswer = "Une question ?";
+  } else if (document.getElementById("project-2").checked) {
+    let objectAnswer = "Un projet dans les cartons";
+  } else if (document.getElementById("project-3").checked) {
+    let objectAnswer = "Seulement envie de dire bonjour";
   }
-  var messageAnswer = document.getElementById('messageAnswer').value;
-
-  console.log(nameAnswer + ' / ' + mailAnswer + ' / ' + objectAnswer + ' / ' + messageAnswer);
+  let messageAnswer = document.getElementById("messageAnswer").value;
 
   $.ajax({
-      url: 'sendEmail.php',
-      data: {
-        name: nameAnswer, 
-        mail: mailAnswer, 
-        object: objectAnswer, 
-        message: messageAnswer
-      }
-    }).done(function () {
-        console.log('Email sended');
+    url: "sendEmail.php",
+    data: {
+      name: nameAnswer,
+      mail: mailAnswer,
+      object: objectAnswer,
+      message: messageAnswer
+    }
+  }).done(function() {
+    console.log("Email sended");
   });
 }
 
@@ -171,7 +254,7 @@ function sendEmail() {
 //	MAIN FUNCTION
 //-------------------------------------
 function main() {
-  document.querySelector('.btn__work').addEventListener('click', seeMyWork);
+  document.querySelector(".btn__work").addEventListener("click", seeMyWork);
 }
 
 document.onload = main();
