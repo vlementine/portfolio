ffd7a193308971c9240609a0c5b153a0d9862db7
Clementine Bertrand <bertrand_clementine@yahoo.fr>
2018-04-15 23:16:04 +0200

    Change CSS+JS for responsive

--- script.js
+++ script.js
@@ -2,11 +2,13 @@
 //	JSON
 //-------------------------------------
 let windowWidth = window.innerWidth;
-const projects = [{
+const projects = [
+  {
     number: "01",
     title: "Back to your dream",
     keywords: "User Interface • Animation",
-    description: "Option A, you kill me right here and now. Apparently I've made that very easy for you. You can kill me, no witnesses and then spend the next few weeks or months tracking down Jesse Pinkman and you kill him too. A pointless exercise it seems…",
+    description:
+      "Option A, you kill me right here and now. Apparently I've made that very easy for you. You can kill me, no witnesses and then spend the next few weeks or months tracking down Jesse Pinkman and you kill him too. A pointless exercise it seems…",
     image: "btyd",
     href: " "
   },
@@ -14,7 +16,8 @@ const projects = [{
     number: "02",
     title: "Le Blog Zenchef",
     keywords: "User Interface • Blog",
-    description: "Option A, you kill me right here and now. Apparently I've made that very easy for you. You can kill me, no witnesses and then spend the next few weeks or months tracking down Jesse Pinkman and you kill him too. A pointless exercise it seems…",
+    description:
+      "Option A, you kill me right here and now. Apparently I've made that very easy for you. You can kill me, no witnesses and then spend the next few weeks or months tracking down Jesse Pinkman and you kill him too. A pointless exercise it seems…",
     image: "blog-zenchef",
     href: " "
   },
@@ -22,7 +25,8 @@ const projects = [{
     number: "03",
     title: "Alcatraz, 1962",
     keywords: "User Interface • Webdoc",
-    description: "Option A, you kill me right here and now. Apparently I've made that very easy for you. You can kill me, no witnesses and then spend the next few weeks or months tracking down Jesse Pinkman and you kill him too. A pointless exercise it seems…",
+    description:
+      "Option A, you kill me right here and now. Apparently I've made that very easy for you. You can kill me, no witnesses and then spend the next few weeks or months tracking down Jesse Pinkman and you kill him too. A pointless exercise it seems…",
     image: "btyd",
     href: " "
   },
@@ -30,13 +34,15 @@ const projects = [{
     number: "04",
     title: "La Maroquinerie",
     keywords: "User Interface • Concert",
-    description: "Option A, you kill me right here and now. Apparently I've made that very easy for you. You can kill me, no witnesses and then spend the next few weeks or months tracking down Jesse Pinkman and you kill him too. A pointless exercise it seems…",
+    description:
+      "Option A, you kill me right here and now. Apparently I've made that very easy for you. You can kill me, no witnesses and then spend the next few weeks or months tracking down Jesse Pinkman and you kill him too. A pointless exercise it seems…",
     image: "btyd",
     href: " "
   }
 ];
 
-const nav = [{
+const nav = [
+  {
     number: "01",
     title: "Home"
   },
@@ -91,20 +97,21 @@ function getAnswer(name, groupBubble, nbBubble) {
 //-------------------------------------
 //	AUTO-REZISE © Zenchef
 //-------------------------------------
-const textareaChat = document.querySelector('#messageAnswer')
-textareaChat.addEventListener('input', autoSize, false);
+const textareaChat = document.querySelector("#messageAnswer");
+textareaChat.addEventListener("input", autoSize, false);
 const TEXTAREA_CONFIG = {
   LINE_HEIGHT: 18, // adjust textarea lineheight
-  PADDING: 0  // adjust textarea padding vertical
-}
+  PADDING: 0 // adjust textarea padding vertical
+};
 function autoSize() {
   if (textareaChat) {
-    textareaChat.setAttribute('rows', 2)
+    textareaChat.setAttribute("rows", 2);
     const rowsRequired = parseInt(
-      (textareaChat.scrollHeight - TEXTAREA_CONFIG.PADDING) / TEXTAREA_CONFIG.LINE_HEIGHT
-    )
-    if (rowsRequired !== parseInt(textareaChat.getAttribute('rows'))) {
-      textareaChat.setAttribute('rows', rowsRequired)
+      (textareaChat.scrollHeight - TEXTAREA_CONFIG.PADDING) /
+        TEXTAREA_CONFIG.LINE_HEIGHT
+    );
+    if (rowsRequired !== parseInt(textareaChat.getAttribute("rows"))) {
+      textareaChat.setAttribute("rows", rowsRequired);
     }
   }
 }
@@ -114,15 +121,26 @@ function autoSize() {
 //-------------------------------------
 function displayBubble(groupBubble, nbBubble) {
   for (let n = 0; n < 2; n++) {
-    document.querySelectorAll(".bubble--group:nth-child(" + groupBubble + ") .bubble")[n].classList.add("enable");
-    document.querySelector(".contact__form form").scrollTo(0, document.body.scrollHeight);
+    document
+      .querySelectorAll(".bubble--group:nth-child(" + groupBubble + ") .bubble")
+      [n].classList.add("enable");
+    document
+      .querySelector(".contact__form form")
+      .scrollTo(0, document.body.scrollHeight);
   }
 
   if (nbBubble == 3) {
     for (let n = 0; n < nbBubble; n++) {
       document
-        .querySelectorAll(".bubble--group:nth-child(" + groupBubble + ") .bubble--group__choice .bubble")[n].classList.add("enable__choice");
-      document.querySelector(".contact__form form").scrollTo(0, document.body.scrollHeight);
+        .querySelectorAll(
+          ".bubble--group:nth-child(" +
+            groupBubble +
+            ") .bubble--group__choice .bubble"
+        )
+        [n].classList.add("enable__choice");
+      document
+        .querySelector(".contact__form form")
+        .scrollTo(0, document.body.scrollHeight);
     }
   }
 }
@@ -131,34 +149,60 @@ function displayBubble(groupBubble, nbBubble) {
 //	DISPLAY PROJECT
 //-------------------------------------
 function displayProject(project) {
-  let imgNumber = document.querySelectorAll(project + " .project__visuel img").length;
+  let imgNumber = document.querySelectorAll(project + " .project__visuel img")
+    .length;
   for (let i = 0; i < imgNumber; i++) {
-    document.querySelectorAll(project + " .project__visuel img")[i].classList.add("enable");
+    document
+      .querySelectorAll(project + " .project__visuel img")
+      [i].classList.add("enable");
   }
 
   document.querySelector(project + " .project__visuel").classList.add("enable");
-  document.querySelector(project + " .description__number").classList.add("enable");
-  document.querySelector(project + " .description__title h2").classList.add("enable");
-  document.querySelector(project + " .description__info span").classList.add("enable");
+  document
+    .querySelector(project + " .description__number")
+    .classList.add("enable");
+  document
+    .querySelector(project + " .description__title h2")
+    .classList.add("enable");
+  document
+    .querySelector(project + " .description__info p")
+    .classList.add("enable");
 
   document.querySelector(project + " .description p").classList.add("enable");
-  document.querySelector(project + " .description__wrapper .btn").classList.add("enable");
+  document
+    .querySelector(project + " .description__wrapper .btn")
+    .classList.add("enable");
 }
 
 function hideProject(project) {
-  let imgNumber = document.querySelectorAll(project + " .project__visuel img").length;
+  let imgNumber = document.querySelectorAll(project + " .project__visuel img")
+    .length;
   for (let i = 0; i < imgNumber; i++) {
-    document.querySelectorAll(project + " .project__visuel img")[i].classList.remove("enable");
+    document
+      .querySelectorAll(project + " .project__visuel img")
+      [i].classList.remove("enable");
   }
 
-  document.querySelector(project + " .project__visuel").classList.remove("enable");
-  document.querySelector(project + " .description__number").classList.remove("enable");
-  document.querySelector(project + " .description__title h2").classList.remove("enable");
+  document
+    .querySelector(project + " .project__visuel")
+    .classList.remove("enable");
+  document
+    .querySelector(project + " .description__number")
+    .classList.remove("enable");
+  document
+    .querySelector(project + " .description__title h2")
+    .classList.remove("enable");
 
-  document.querySelector(project + " .description__info span").classList.remove("enable");
+  document
+    .querySelector(project + " .description__info p")
+    .classList.remove("enable");
 
-  document.querySelector(project + " .description p").classList.remove("enable");
-  document.querySelector(project + " .description__wrapper .btn").classList.remove("enable");
+  document
+    .querySelector(project + " .description p")
+    .classList.remove("enable");
+  document
+    .querySelector(project + " .description__wrapper .btn")
+    .classList.remove("enable");
 }
 
 function seeMyWork() {
@@ -178,7 +222,9 @@ function navActiveItem(param) {
   for (let n = 0; n < Object.keys(nav).length; n++) {
     document.querySelectorAll("span.nav__item")[n].classList.remove("active");
   }
-  document.querySelector("span.nav__item:nth-child(" + param + ")").classList.add("active");
+  document
+    .querySelector("span.nav__item:nth-child(" + param + ")")
+    .classList.add("active");
 }
 
 //-------------------------------------
@@ -193,17 +239,23 @@ function navNew(param) {
   let itemClicked = parseInt(param);
 
   //Item active
-  let projectActive = parseInt(document.querySelector(".project.enable").id.substr(-1));
+  let projectActive = parseInt(
+    document.querySelector(".project.enable").id.substr(-1)
+  );
 
   navActiveItem(projectClicked + 1);
 
   let idToDisplay = "#project-0" + projectClicked;
   let idToHide = "#" + document.querySelector(".project.enable").id;
 
-  if (projectClicked < totalItemNav - 1 && projectClicked !== 0 && projectClicked !== projectActive) {
+  if (
+    projectClicked < totalItemNav - 1 &&
+    projectClicked !== 0 &&
+    projectClicked !== projectActive
+  ) {
     hideProject(idToHide);
 
-    setTimeout(function () {
+    setTimeout(function() {
       document.querySelector(idToHide).classList.remove("enable");
       document.querySelector(idToDisplay).classList.add("enable");
       displayProject(idToDisplay);
@@ -212,17 +264,21 @@ function navNew(param) {
     document.querySelector(".home").classList.remove("disable");
     hideProject(idToHide);
 
-    setTimeout(function () {
+    setTimeout(function() {
       document.querySelector(idToHide).classList.remove("enable");
     }, 700);
   } else if (itemClicked == totalItemNav) {
     document.querySelector(".contact__wrapper").classList.remove("disable");
     hideProject(idToHide);
 
-    setTimeout(function () {
+    setTimeout(function() {
       document.querySelector(idToHide).classList.remove("enable");
-      document.querySelector(".bubble--group:nth-child(1) .bubble").classList.add("enable");
-      document.querySelector(".bubble--group:nth-child(1) .bubble--answer").classList.add("enable");
+      document
+        .querySelector(".bubble--group:nth-child(1) .bubble")
+        .classList.add("enable");
+      document
+        .querySelector(".bubble--group:nth-child(1) .bubble--answer")
+        .classList.add("enable");
     }, 700);
   }
 }
@@ -245,7 +301,12 @@ function displayMenu() {
 function navMenu(param) {
   let hideProject = document
     .querySelectorAll(".project")
-    .forEach(element => (element.classList.contains("enable") ? element.classList.remove("enable") : ""));
+    .forEach(
+      element =>
+        element.classList.contains("enable")
+          ? element.classList.remove("enable")
+          : ""
+    );
   displayMenu();
   if (param == "home") {
     hideProject;
@@ -256,7 +317,7 @@ function navMenu(param) {
     document.querySelector(".home").classList.add("disable");
     document.querySelector(".contact__wrapper").classList.add("disable");
     navActiveItem(2);
-    setTimeout(function () {
+    setTimeout(function() {
       document.querySelector("#project-01").classList.add("enable");
       displayProject("#project-01");
     }, 500);
@@ -264,9 +325,13 @@ function navMenu(param) {
     hideProject;
     document.querySelector(".contact__wrapper").classList.remove("disable");
 
-    setTimeout(function () {
-      document.querySelector(".bubble--group:nth-child(1) .bubble").classList.add("enable");
-      document.querySelector(".bubble--group:nth-child(1) .bubble--answer").classList.add("enable");
+    setTimeout(function() {
+      document
+        .querySelector(".bubble--group:nth-child(1) .bubble")
+        .classList.add("enable");
+      document
+        .querySelector(".bubble--group:nth-child(1) .bubble--answer")
+        .classList.add("enable");
       document.querySelector(".home").classList.add("disable");
     }, 500);
   }
@@ -340,7 +405,9 @@ function responsive() {
                 project.image
               }_visuel-home.png" alt="Visuel du Blog Zenchef" draggable="false">
             </div>
-            <img src="img/${project.image}_letter.png" alt="B" draggable="false">
+            <img src="img/${
+              project.image
+            }_letter.png" alt="B" draggable="false">
           </div>
 
           <div class="project__description">
@@ -350,7 +417,7 @@ function responsive() {
               </div>
               <span class="description__number">${project.number}.</span>
               <div class="description__info">
-                <span>${project.keywords}</span>
+                <p>${project.keywords}</p>
               </div>
               <div class="description">
                 <p>${project.description}</p>
@@ -362,40 +429,49 @@ function responsive() {
   });
 
   projects.forEach(projectResponsive => {
-    contentResponsive += `<a href="${projectResponsive.href}" class="project" id="project-${projectResponsive.number}">
-          <div class="project__visuel">
-            <div class="visuel__mockup">
+    contentResponsive += `<div class="project--mobile" id="project-${
+      projectResponsive.number
+    }">
+    <div class="project__content">
+      <div class="project__description--mobile">
+          <div>
+            <div class="description__title--mobile">
+                <h2> ${projectResponsive.title} </h2>
+            </div>
+            <div class="description__number-wrapper--mobile">
+              <span class="description__number--mobile">${
+                projectResponsive.number
+              }.</span>
+              <span class="description__number-shadow--mobile">${
+                projectResponsive.number
+              }.</span>
+            </div>
+          </div>
+          <div class="description__info--mobile">
+              <p>${projectResponsive.keywords}</p>
+          </div>
+      </div>
+      <div class="project__visuel--mobile">
+          <div class="visuel__mockup">
               <img class="mockup--desktop" src="img/${
                 projectResponsive.image
               }_visuel-home.png" alt="Visuel du Blog Zenchef" draggable="false">
-            </div>
-            <img src="img/${projectResponsive.image}_letter.png" alt="B" draggable="false">
           </div>
-
-          <div class="project__description">
-            <div class="description__wrapper">
-              <div class="description__title">
-                <h2> ${projectResponsive.title} </h2>
-              </div>
-              <span class="description__number">${projectResponsive.number}.</span>
-              <div class="description__info">
-                <div class="info">
-                  <span>Rôle</span>
-                  <p>${projectResponsive.keywords}</p>
-                </div>
-              </div>
-              <div class="description">
-                <p>${projectResponsive.description}</p>
-              </div>
-            </div>
-          </div>
-        </a>`;
+          <img class="visuel__letter" src="img/${
+            projectResponsive.image
+          }_letter.png" alt="B" draggable="false">
+      </div>
+      <a class="btn" href="${projectResponsive.href}">Découvrir le projet</a>
+    </div>
+    <div class="nav-nav">
+    </div>
+</div>`;
   });
 
   if (windowWidth <= 650) {
     console.log("plop");
     document.getElementById("projects").innerHTML = contentResponsive;
-    document.querySelector(".home").classList.remove("disable");
+    document.querySelector(".home").classList.add("disable");
   } else {
     console.log("ploup");
     document.getElementById("projects").innerHTML = content;
