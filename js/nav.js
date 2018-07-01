2a7d1d0c36f6cb6c8c1fbcb2ace2a8c704efa360
Clementine Bertrand <bertrand_clementine@yahoo.fr>
2018-07-01 19:31:02 +0200

    Generate id from url for content page

--- js/nav.js
+++ js/nav.js
@@ -144,7 +144,7 @@ function navDesktop(direction = null, param = null) {
   //Display projects
   if (projectClicked < totalItemNav - 1 && projectClicked !== 0 && projectClicked !== projectActive) {
     hideProject(idToHide);
-    setTimeout(function() {
+    setTimeout(function () {
       document.querySelector(idToHide).classList.remove('enable');
       document.querySelector(idToDisplay).classList.add('enable');
       displayProject(idToDisplay);
@@ -154,14 +154,14 @@ function navDesktop(direction = null, param = null) {
   } else if (projectClicked === 0) {
     document.querySelector('.home').classList.remove('disable');
     hideProject(idToHide);
-    setTimeout(function() {
+    setTimeout(function () {
       document.querySelector(idToHide).classList.remove('enable');
     }, 700);
     //Display contact
   } else if (itemClicked == totalItemNav) {
     document.querySelector('.contact__wrapper').classList.remove('disable');
     hideProject(idToHide);
-    setTimeout(function() {
+    setTimeout(function () {
       document.querySelector(idToHide).classList.remove('enable');
       document.querySelector('.bubble--group:nth-child(1) .bubble').classList.add('enable');
       document.querySelector('.bubble--group:nth-child(1) .bubble--answer').classList.add('enable');
@@ -188,7 +188,7 @@ function navMobile(param) {
   if (itemClicked < totalItemNav && itemClicked !== 1) {
     document.querySelector('.number--active').textContent = '0' + itemClicked;
     hideProject(idToHide);
-    setTimeout(function() {
+    setTimeout(function () {
       document.querySelector(idToHide).classList.remove('enable');
       document.querySelector(idToDisplay).classList.add('enable');
       displayProject(idToDisplay);
@@ -246,7 +246,7 @@ function navMenu(param) {
     //Work
     document.querySelector('.home').classList.add('disable');
     document.querySelector('.contact__wrapper').classList.add('disable');
-    setTimeout(function() {
+    setTimeout(() => {
       document.querySelector('#project-01').classList.add('enable');
       displayProject('#project-01');
     }, 500);
@@ -271,29 +271,26 @@ function navMenu(param) {
 
 menuHoverProject = (number, name) => {
   document.querySelector('.menu__project-visuel').classList.add('hide');
-  setTimeout(function() {
-    let urlPlop = window.location.href;
-    let positionSlash = urlPlop.lastIndexOf('/') + 1;
-    let imgPath = urlPlop.substring(positionSlash) === 'index.html' ? './img/' : '../img/';
+  setTimeout(() => {
+    let urlPage = window.location.href;
+    let positionSlash = urlPage.lastIndexOf('/') + 1;
+    let imgPath = urlPage.substring(positionSlash) === 'index.html' ? './img/' : '../img/';
 
     document.querySelector('.menu__project-visuel .mockup--desktop').src = imgPath + name + '_visuel-home.png';
     document.querySelector('.menu__project-visuel .visuel__mockup+img').src = imgPath + name + '_letter.png';
     document.querySelector('.menu__project-visuel').id = 'menu__project-0' + number;
   }, 500);
-  setTimeout(function() {
+  setTimeout(() => {
     document.querySelector('.menu__project-visuel').classList.remove('hide');
   }, 600);
 }
 
-function redirectToPageProject(varURL) {
+redirectToPageProject = (varURL) => {
   document.querySelector('.white-screen').classList.add('redirect-page');
   setTimeout(() => {
-    let urlPlop = window.location.href;
-    let positionSlash = urlPlop.lastIndexOf('/') + 1;
-    if (urlPlop.substring(positionSlash) === 'index.html') {
-      window.location.href = './projects-page/project-page--' + varURL + '.html';
-    } else {
-      window.location.href = './project-page--' + varURL + '.html';
-    }
+    let urlPage = window.location.href;
+    let positionSlash = urlPage.lastIndexOf('/') + 1;
+    let urlPath = urlPage.substring(positionSlash) === 'index.html' ? './projects-page/project-page--' : './project-page--'
+    window.location.href = urlPath + varURL + '.html';
   }, 1500);
 }
--- js/script-project.js
+++ js/script-project.js
@@ -1,13 +1,11 @@
 //-------------------------------------
 //	JSON
 //-------------------------------------
-const projects = [
-  {
+const projects = [{
     number: '01',
     title: 'Back to your dream',
     keywords: 'User Interface • Animation',
-    description:
-      "Option A, you kill me right here and now. Apparently I've made that very easy for you. You can kill me, no witnesses and then spend the next few weeks or months tracking down Jesse Pinkman and you kill him too. A pointless exercise it seems…",
+    description: "Option A, you kill me right here and now. Apparently I've made that very easy for you. You can kill me, no witnesses and then spend the next few weeks or months tracking down Jesse Pinkman and you kill him too. A pointless exercise it seems…",
     image: 'btyd',
     link: ' '
   },
@@ -15,8 +13,7 @@ const projects = [
     number: '02',
     title: 'Le Blog Zenchef',
     keywords: 'User Interface • Blog',
-    description:
-      "Option A, you kill me right here and now. Apparently I've made that very easy for you. You can kill me, no witnesses and then spend the next few weeks or months tracking down Jesse Pinkman and you kill him too. A pointless exercise.",
+    description: "Option A, you kill me right here and now. Apparently I've made that very easy for you. You can kill me, no witnesses and then spend the next few weeks or months tracking down Jesse Pinkman and you kill him too. A pointless exercise.",
     image: 'blog-zenchef',
     link: ' '
   },
@@ -24,8 +21,7 @@ const projects = [
     number: '03',
     title: 'Alcatraz, 1962',
     keywords: 'User Interface • Webdoc',
-    description:
-      "Option A, you kill me right here and now. Apparently I've made that very easy for you. You can kill me, no witnesses and then spend the next few weeks or months tracking down Jesse Pinkman and you kill him too. A pointless exercise it seems…",
+    description: "Option A, you kill me right here and now. Apparently I've made that very easy for you. You can kill me, no witnesses and then spend the next few weeks or months tracking down Jesse Pinkman and you kill him too. A pointless exercise it seems…",
     image: 'alcatraz',
     link: ' '
   },
@@ -33,8 +29,7 @@ const projects = [
     number: '04',
     title: 'La Maroquinerie',
     keywords: 'User Interface • Concert',
-    description:
-      "Option A, you kill me right here and now. Apparently I've made that very easy for you. You can kill me, no witnesses and then spend the next few weeks or months tracking down Jesse Pinkman and you kill him too.",
+    description: "Option A, you kill me right here and now. Apparently I've made that very easy for you. You can kill me, no witnesses and then spend the next few weeks or months tracking down Jesse Pinkman and you kill him too.",
     image: 'maroquinerie',
     link: ' '
   }
@@ -94,8 +89,15 @@ function main() {
   document.querySelector('.project__img img').classList.add('enable');
 
 
-  //Traiter l'url pour générer le bon contenu automatiuement
-  displayContentPage(0);
+  //Traiter l'url pour générer le bon contenu automatiquement
+
+  //Generate good project ID from URL
+  let initialURL = window.location.href;
+  let positionAfterHyphen = initialURL.lastIndexOf('-') + 1;
+  let idProject = initialURL.substring(positionAfterHyphen, (positionAfterHyphen + 1)) - 1;
+  console.log(idProject);
+
+  displayContentPage(idProject);
 }
 
 const displayContentPage = idProject => {
