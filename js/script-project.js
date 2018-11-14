61b03f227b7cd860ac3e21967ceb7eb5fb9a7b1d
Clementine Bertrand <bertrand_clementine@yahoo.fr>
2018-11-14 14:26:39 +0100

    Add slider

--- js/script-project.js
+++ js/script-project.js
@@ -2,67 +2,122 @@
 //	GENERATE CONTENT PAGE
 //-------------------------------------
 const displayContentPage = idProject => {
-  document.querySelector('h1').innerText = projects[idProject].title;
-  document.querySelector('p.subtitle').innerText = projects[idProject].keywords;
-  document.querySelector('.project-intro p').innerText = projects[idProject].description;
-  document.querySelector('.project-intro a.btn').href = projects[idProject].link;
-  // document.querySelector('.project__img img').src = '../img/' + projects[idProject].image + '_project-01.png';
-  //document.querySelector('.next-project__visuel img').src = '../img/' + projects[idProject].image + '_next-project.png';
+	document.querySelector('h1').innerText = projects[idProject].title;
+	document.querySelector('p.subtitle').innerText = projects[idProject].keywords;
+	document.querySelector('.project-intro p').innerText =
+		projects[idProject].description;
+	document.querySelector('.project-intro a.btn').href =
+		projects[idProject].link;
+	// document.querySelector('.project__img img').src = '../img/' + projects[idProject].image + '_project-01.png';
+	//document.querySelector('.next-project__visuel img').src = '../img/' + projects[idProject].image + '_next-project.png';
 
-  generateMenu();
+	generateMenu();
 };
 
 //-------------------------------------
 //	REDIRECTION -> INDEX
 //-------------------------------------
 const redirectionProject = varURL => {
-  document.querySelector('.white-screen').style.display = 'block';
-  document.querySelector('.white-screen').classList.add('redirect-page');
-  setTimeout(() => {
-    window.location.href = '../index.html#' + varURL;
-  }, 1500);
+	document.querySelector('.white-screen').style.display = 'block';
+	document.querySelector('.white-screen').classList.add('redirect-page');
+	setTimeout(() => {
+		window.location.href = '../index.html#' + varURL;
+	}, 1500);
+};
+
+//-------------------------------------
+//	SLIDER
+//-------------------------------------
+const calcHeightSlider = () => {
+	document.querySelector(
+		'.slider__wrapper'
+	).style.height = document.querySelector('.slider__pictures').clientHeight;
+};
+
+const slider = number => {
+	let slide = document.querySelector('.slide');
+	let slideW = slide.clientWidth;
+	let slideM =
+		parseInt(getComputedStyle(slide).marginLeft) +
+		parseInt(getComputedStyle(slide).marginRight);
+	let calcTranslate =
+		'calc((-60vw * ' + number + ') - (' + slideM + 'px * ' + number + '))';
+
+  console.log(calcTranslate);
+
+	let bulletLength = document.querySelectorAll('.slider-nav__bullet').length;
+
+	// Remove class active to all bullets
+	for (let i = 0; i < bulletLength; i++) {
+		document
+			.querySelectorAll('.slider-nav__bullet')
+			[i].classList.remove('bullet--active');
+	}
+
+	// Add active class to bullet
+	document
+		.querySelector('.slider-nav__bullet:nth-child(' + (number + 1) + ')')
+		.classList.add('bullet--active');
+
+	// Move slides
+	document.querySelector('.slider__pictures').style.transform =
+		'translate(' + calcTranslate + ')';
+};
+
+//-------------------------------------
+//	RESPONSIVE
+//-------------------------------------
+const responsive = () => {
+	calcHeightSlider();
 };
 
 //-------------------------------------
 //	MAIN FUNCTION
 //-------------------------------------
 const main = () => {
-  //Display
-  // document.querySelector('header').classList.add('enable');
-  // document.querySelector('.project__summary h1').classList.add('enable');
-  // document.querySelector('.type--shadow').classList.add('enable');
-  // for (let i = 0; i < 3; i++) {
-  //   document.querySelectorAll('.project-page .project__info .info span')[i].classList.add('enable');
-  //   document.querySelectorAll('.project-page .project__info .info p')[i].classList.add('enable');
-  // }
-  // document.querySelector('.project__intro').classList.add('enable');
-  // document.querySelector('.project__img span').classList.add('enable');
-  // document.querySelector('.project__img img').classList.add('enable');
+	//Display
+	// document.querySelector('header').classList.add('enable');
+	// document.querySelector('.project__summary h1').classList.add('enable');
+	// document.querySelector('.type--shadow').classList.add('enable');
+	// for (let i = 0; i < 3; i++) {
+	//   document.querySelectorAll('.project-page .project__info .info span')[i].classList.add('enable');
+	//   document.querySelectorAll('.project-page .project__info .info p')[i].classList.add('enable');
+	// }
+	// document.querySelector('.project__intro').classList.add('enable');
+	// document.querySelector('.project__img span').classList.add('enable');
+	// document.querySelector('.project__img img').classList.add('enable');
 
-  //Generate good project ID from URL
-  let initialURL = window.location.href;
-  let positionAfterHyphen = initialURL.lastIndexOf('/') + 1;
-  let positionHtml = initialURL.lastIndexOf('.html') + 1;
-  let nameProject = initialURL.substring(positionAfterHyphen, positionHtml - 1);
+	//Generate good project ID from URL
+	let initialURL = window.location.href;
+	let positionAfterHyphen = initialURL.lastIndexOf('/') + 1;
+	let positionHtml = initialURL.lastIndexOf('.html') + 1;
+	let nameProject = initialURL.substring(positionAfterHyphen, positionHtml - 1);
 
-  let idProject = 0;
+	//Height of slider
+	calcHeightSlider();
 
-  switch (nameProject) {
-    case 'back-to-your-dream':
-      idProject = 0;
-      break;
-    case 'blog-zenchef':
-      idProject = 1;
-      break;
-    case 'alcatraz-1962':
-      idProject = 2;
-      break;
-    case 'le-trianon':
-      idProject = 3;
-      break;
-  }
+	let idProject = 0;
 
-  displayContentPage(idProject);
+	switch (nameProject) {
+		case 'back-to-your-dream':
+			idProject = 0;
+			break;
+		case 'blog-zenchef':
+			idProject = 1;
+			break;
+		case 'alcatraz-1962':
+			idProject = 2;
+			break;
+		case 'le-trianon':
+			idProject = 3;
+			break;
+	}
+
+	displayContentPage(idProject);
 };
 
+//-------------------------------------
+//	INITIALIZE
+//-------------------------------------
 document.onload = main();
+window.onresize = responsive;
