fdf092c7db4621153b936b31e6acf9c1b366445f
Clementine Bertrand <bertrand_clementine@yahoo.fr>
2019-03-05 20:39:19 +0100

    I don't remember...

--- js/chat.js
+++ js/chat.js
@@ -2,36 +2,44 @@
 //	DISPLAY BUBBLE
 //-------------------------------------
 const displayBubble = (groupBubble, nbBubble) => {
-  for (let n = 0; n < 2; n++) {
-    document.querySelectorAll('.bubble--group:nth-child(' + groupBubble + ') .bubble')[n].classList.add('enable');
-    document.querySelector('.contact__form form').scrollTo(0, document.querySelector('.contact__form form').scrollHeight);
-  }
+	for (let n = 0; n < 2; n++) {
+		document
+			.querySelectorAll('.bubble--group:nth-child(' + groupBubble + ') .bubble')
+			[n].classList.add('enable');
+		document
+			.querySelector('.contact__form form')
+			.scrollTo(0, document.querySelector('.contact__form form').scrollHeight);
+	}
 
-  if (nbBubble == 3) {
-    for (let n = 0; n < nbBubble; n++) {
-      document
-        .querySelectorAll('.bubble--group:nth-child(' + groupBubble + ') .bubble--group__choice .bubble')
-        [n].classList.add('enable__choice');
-      document.querySelector('.contact__form form').scrollTo(0, document.querySelector('.contact__form form').scrollHeight);
-    }
-  }
-}
+	if (nbBubble == 3) {
+		for (let n = 0; n < nbBubble; n++) {
+			document
+				.querySelectorAll(
+					'.bubble--group:nth-child(' + groupBubble + ') .bubble--group__choice .bubble'
+				)
+				[n].classList.add('enable__choice');
+			document
+				.querySelector('.contact__form form')
+				.scrollTo(0, document.querySelector('.contact__form form').scrollHeight);
+		}
+	}
+};
 
 //-------------------------------------
 //	GET ANSWER
 //-------------------------------------
 const getAnswer = (name, groupBubble, nbBubble) => {
-  let answer = document.getElementById(name + 'Answer').value;
-  displayBubble(groupBubble, nbBubble);
+	let answer = document.getElementById(name + 'Answer').value;
+	displayBubble(groupBubble, nbBubble);
 
-  if (answer !== '' && answer !== ' ') {
-    document.getElementById(name + 'Answer').setAttribute('readonly', true);
+	if (answer !== '' && answer !== ' ') {
+		document.getElementById(name + 'Answer').setAttribute('readonly', true);
 
-    if (name == 'name') {
-      document.getElementById(name + 'Question').innerHTML = answer;
-    }
-  }
-}
+		if (name == 'name') {
+			document.getElementById(name + 'Question').innerHTML = answer;
+		}
+	}
+};
 
 //-------------------------------------
 //	AUTO-REZISE © Zenchef
@@ -39,68 +47,70 @@ const getAnswer = (name, groupBubble, nbBubble) => {
 const textareaChat = document.querySelector('#messageAnswer');
 textareaChat.addEventListener('input', autoSize, false);
 const TEXTAREA_CONFIG = {
-  LINE_HEIGHT: 18, 
-  PADDING: 0 
+	LINE_HEIGHT: 18,
+	PADDING: 0
 };
 function autoSize() {
-  if (textareaChat) {
-    textareaChat.setAttribute('rows', 2);
-    const rowsRequired = parseInt((textareaChat.scrollHeight - TEXTAREA_CONFIG.PADDING) / TEXTAREA_CONFIG.LINE_HEIGHT);
-    if (rowsRequired !== parseInt(textareaChat.getAttribute('rows'))) {
-      textareaChat.setAttribute('rows', rowsRequired);
-    }
-  }
+	if (textareaChat) {
+		textareaChat.setAttribute('rows', 2);
+		const rowsRequired = parseInt(
+			(textareaChat.scrollHeight - TEXTAREA_CONFIG.PADDING) / TEXTAREA_CONFIG.LINE_HEIGHT
+		);
+		if (rowsRequired !== parseInt(textareaChat.getAttribute('rows'))) {
+			textareaChat.setAttribute('rows', rowsRequired);
+		}
+	}
 }
 
 //-------------------------------------
 //    VALIDATE EMAIL
 //-------------------------------------
-const validateEmail = (email) => {
-  let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(email);
-}
+const validateEmail = email => {
+	let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(email);
+};
 
 const validate = (groupBubble, nbBubble) => {
-  let result = document.querySelector('#result');
-  let email = document.querySelector('#mailAnswer').value;
+	let result = document.querySelector('#result');
+	let email = document.querySelector('#mailAnswer').value;
 
-  if (validateEmail(email)) {
-    document.querySelector('#mailAnswer').setAttribute('readonly', true);
-    result.style.display = 'none';
-    displayBubble(groupBubble, nbBubble);
-    sendEmail();
-  } else {
-    result.innerText = "L'adresse n'est pas valide";
-    result.style.color = 'crimson';
-  }
-  return false;
-}
+	if (validateEmail(email)) {
+		document.querySelector('#mailAnswer').setAttribute('readonly', true);
+		result.style.display = 'none';
+		displayBubble(groupBubble, nbBubble);
+		sendEmail();
+	} else {
+		result.innerText = "L'adresse n'est pas valide";
+		result.style.color = 'crimson';
+	}
+	return false;
+};
 
 //-------------------------------------
 //    MAIL SENDING
 //-------------------------------------
 const sendEmail = () => {
-  //Variables
-  let nameAnswer = document.getElementById('nameAnswer').value;
-  let mailAnswer = document.getElementById('mailAnswer').value;
-  if (document.getElementById('project-1').checked) {
-    var objectAnswer = 'Une question ?';
-  } else if (document.getElementById('project-2').checked) {
-    var objectAnswer = 'Un projet dans les cartons';
-  } else if (document.getElementById('project-3').checked) {
-    var objectAnswer = 'Seulement envie de dire bonjour';
-  }
-  let messageAnswer = document.getElementById('messageAnswer').value;
+	//Variables
+	let nameAnswer = document.getElementById('nameAnswer').value;
+	let mailAnswer = document.getElementById('mailAnswer').value;
+	if (document.getElementById('project-1').checked) {
+		var objectAnswer = 'Une question ?';
+	} else if (document.getElementById('project-2').checked) {
+		var objectAnswer = 'Un projet dans les cartons';
+	} else if (document.getElementById('project-3').checked) {
+		var objectAnswer = 'Seulement envie de dire bonjour';
+	}
+	let messageAnswer = document.getElementById('messageAnswer').value;
 
-  xhr = new XMLHttpRequest();
-  xhr.open('POST', 'sendEmail.php');
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.send(
-    JSON.stringify({
-      name: nameAnswer,
-      mail: mailAnswer,
-      object: objectAnswer,
-      message: messageAnswer
-    })
-  );
-}
+	xhr = new XMLHttpRequest();
+	xhr.open('POST', 'sendEmail.php');
+	xhr.setRequestHeader('Content-Type', 'application/json');
+	xhr.send(
+		JSON.stringify({
+			name: nameAnswer,
+			mail: mailAnswer,
+			object: objectAnswer,
+			message: messageAnswer
+		})
+	);
+};
