68f427c0af37ec94e7b008a5c584173afa098cec
Clementine Bertrand <bertrand_clementine@yahoo.fr>
2019-03-22 19:28:22 +0100

    Fix url project

--- js/data-json.js
+++ js/data-json.js
@@ -21,6 +21,7 @@ const projects = [
 		description:
 			'Le Trianon est une salle de spectacle parisienne située au pied de la butte Montmartre.',
 		image: 'le-trianon',
+		href: 'le-trianon',
 		nextProject: 'Alcatraz, 1962'
 	},
 	{
@@ -30,6 +31,7 @@ const projects = [
 		description:
 			"Alcatraz, 1962 est un web documentaire qui relate les préparatifs ainsi que le déroulement de l'évasion de 3 prisonniers d'Alcatraz. La seule évasion ayant atteint son but depuis l'ouverture de la célèbre prison !",
 		image: 'alcatraz',
+		href: 'alcatraz',
 		nextProject: 'Back To Your Dream'
 	},
 	{
@@ -39,6 +41,7 @@ const projects = [
 		description:
 			'Back To Your Dream, est une plateforme qui vous permet à vous ou à vos proches de réaliser votre rêve ultime.',
 		image: 'btyd',
+		href: 'back-to-your-dream',
 		nextProject: 'Zenchef'
 	}
 ];
